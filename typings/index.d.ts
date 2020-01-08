15214b30fa96a514df384ecd26fa262ff6b2610e
ycbl <ycblone@163.com>
2020-01-08 17:34:49 +0800

    tabBar01.08

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,8 +1,9 @@
 /// <reference path="./types/index.d.ts" />
 
 interface IAppOption {
-  globalData: {
-    userInfo?: WechatMiniprogram.UserInfo,
-  }
-  userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
+    globalData: {
+        userInfo?: WechatMiniprogram.UserInfo,
+        [key: string]: any
+    }
+    userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
 }
